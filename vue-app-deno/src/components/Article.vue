e52b77fdfc318327d44692343e7445ffd9ee8f5c
Roberto <michelettiroberto7@gmail.com>
2024-11-02 23:00:47 +0100

    Feat: adding vue example app

---
+++ vue-app-deno/src/components/Article.vue
@@ -0,0 +1,20 @@
+<template>
+  <article class="card">
+    <img :src="image" :alt="title">
+    <h2>{{ title }}</h2>
+
+    <div>
+      <button type="button" @click="count++">Count is {{ count }}</button>
+    </div>
+  </article>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  image: String,
+  title: String,
+})
+
+const count = ref(0)
+</script>
